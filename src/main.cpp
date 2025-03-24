43d6cace789754f07fe15ab8650cbbda4e56d167
p1k0chu <anatoliyid0207@gmail.com>
2025-03-24 18:53:25 +0200

    some basic stuff in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,19 @@
+#include <cstdlib>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include "main.hpp"
+
 int main() {
+    glfwInit();
+    glfwWindowHint(GLFW_VERSION_MAJOR,  4);
+    glfwWindowHint(GLFW_VERSION_MINOR,  6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+}
+
+void panic() {
+    glfwTerminate();
+    exit(1);
 }
